03aa8773c65ce0b97ac70309ae106f11e578d393
pangio13 <giovannipanunzio@duck.com>
2026-04-04 15:00:18 +0200

    Update sources and improve error handling

--- src/main.py
+++ src/main.py
@@ -6,8 +6,9 @@ import xml.etree.ElementTree as ET
 from io import BytesIO
 
 SOURCES = [
-    {"url": "https://epgshare01.online/epgshare01/epg_ripper_IT1.xml.gz", "is_gz": True, "name": "EPGShare01"},
-    {"url": "https://iptv-epg.org/files/epg-it.xml.gz", "is_gz": True, "name": "IPTV-EPG.org"},
+    {"url": "https://epgshare01.online/epgshare01/epg_ripper_IT1.xml.gz", "is_gz": True, "name": "EPGShare"},
+    {"url": "https://www.open-epg.com/files/italy3.xml", "is_gz": True, "name": "OpenEPG"},
+    {"url": "https://iptv-epg.org/files/epg-it.xml.gz", "is_gz": True, "name": "IPTV-EPG"},
     {"url": "http://epg-guide.com/dtt.xml", "is_gz": False, "name": "EPG-Guide"}
 ]
 
